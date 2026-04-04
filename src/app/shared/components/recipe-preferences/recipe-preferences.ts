bdd28f087f65a3a47a96cd85246b6583c6f697ce
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-04-04 19:53:06 +0200

    preference form started

--- src/app/shared/components/recipe-preferences/recipe-preferences.ts
+++ src/app/shared/components/recipe-preferences/recipe-preferences.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { CounterType } from '../../types/recipe_types';
 
 @Component({
   selector: 'app-recipe-preferences',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recipe-preferences.html',
   styleUrl: './recipe-preferences.scss',
 })
-export class RecipePreferences {}
+export class RecipePreferences {
+
+  portions: number = 1;
+  chefs: number = 1;
+
+  increase(type:CounterType) {
+    if (type === 'portions') {
+      this.portions++;
+    } else if (type === 'chefs'  && this.chefs < 4) {
+      this.chefs++;
+    }
+  }
+
+  decrease(type:CounterType) {
+    if (type === 'portions' && this.portions > 1) {
+      this.portions--;
+    } else if (type === 'chefs' && this.chefs > 1) {
+      this.chefs--;
+    }
+  }
+}
--- src/app/shared/types/recipe_types.ts
+++ src/app/shared/types/recipe_types.ts
@@ -1,1 +1,2 @@
 export type Unit = 'piece' | 'ml' | 'gram';
+export type CounterType = 'portions' | 'chefs';
